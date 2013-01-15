603c4489bb32544ddd1ba4f05388bb8556b7151f
Zinnirah Binti Kasim <zinnirah.kasim@smail.inf.h-brs.de>
2013-01-15 11:14:59 +0100

    trying to upload all

---
+++ build/CMakeFiles/ROSBUILD_genmsg_roseus_kee_use_cases.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_genmsg_roseus_kee_use_cases"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_roseus_kee_use_cases.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
